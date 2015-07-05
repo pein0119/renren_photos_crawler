41da99562e7bcb3e9f9388d449b26f84210fe4c0
pein0119 <pein0119@gmail.com>
2015-07-05 16:42:02 +0800

    相册名中的空格替换为下划线

--- renren_photos_crawler.py
+++ renren_photos_crawler.py
@@ -94,7 +94,7 @@ def get_imgs(album_url_dict, headers):
 
 def download_img(img_dict, album_url_dict, start_dir):
     for album_id, image_list in img_dict.iteritems():
-        cur_dir = start_dir + album_url_dict[album_id]['album_name'].replace(' ', '')
+        cur_dir = start_dir + album_url_dict[album_id]['album_name'].replace(' ', '_')
         
         if not os.path.exists(cur_dir):
             os.makedirs(cur_dir)
